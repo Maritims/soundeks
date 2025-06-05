7fb3d1fe45d71a81ebb4b80b7c8a1f1225b840bb
Maritim <maritim@gmail.com>
2025-06-06 00:19:17 +0200

    Added some tests for English names.

--- src/test/java/io/github/maritims/soundeks/SoundeksTest.java
+++ src/test/java/io/github/maritims/soundeks/SoundeksTest.java
@@ -23,6 +23,18 @@ class SoundeksTest {
         );
     }
 
+    public static Stream<Arguments> transforming_similar_sounding_names_should_have_the_same_result() {
+        return Stream.of(
+                Arguments.arguments("Tommy", "Tommie"),
+                Arguments.arguments("Donny", "Donnie"),
+                Arguments.arguments("Anna", "Anne"),
+                Arguments.arguments("Mike", "Mikey"),
+                Arguments.arguments("Jon", "John"),
+                Arguments.arguments("Erica", "Erika"),
+                Arguments.arguments("Grace", "Grays")
+        );
+    }
+
     @ParameterizedTest
     @MethodSource
     void transform(String original, String expectedResult) {
@@ -34,5 +46,12 @@ class SoundeksTest {
 
         // assert
         assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void transforming_similar_sounding_names_should_have_the_same_result(String name1, String name2) {
+        var sut = new Soundeks();
+        assertEquals(sut.transform(name1), sut.transform(name2), "Unexpected result for " + name1 + " and " + name2);
     }
 }
